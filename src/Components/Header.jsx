7e8a416a8a2f53cc7b8f1c52ee03023d56621614
Michele <mariamichelep@gmail.com>
2023-11-29 22:49:47 -0300

    Alterando Style e acrescentando arquivo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,36 +3,33 @@ import { Link } from 'react-router-dom';
 import "../Style/styleHeader.scss";
 import "../Style/reset.scss";
 import logoMarca from "../assets/logo2.png";
-import "../assets/Michele-Maria.pdf"
 
 
 
 
 function Header() {
+
+  
   return (
     <>
+    <header>
 
-      <header>
+      <figure >
+        <img src={logoMarca} alt='Logomarca com o nome Michele' className='logo' />
+      </figure>
 
-
-
-
-        <figure >
-          <img src={logoMarca} alt='Logomarca com o nome Michele' className='logo' />
-        </figure>
-
-        <button className='button-cv'><a className='link-cv' href="https://drive.google.com/file/d/1ziGkQwkRnoMrPY2InzAyDj1zO4hgPGf0/view?usp=drive_link" target="_black" rel='external'>Download CV</a></button>
+      <button className='button-cv'><a className='link-cv' href="https://drive.google.com/file/d/1ziGkQwkRnoMrPY2InzAyDj1zO4hgPGf0/view?usp=drive_link" target="_black" rel='external'>Download CV</a></button>
 
 
-        <nav className='container'>
-          <Link to="/">Inicio</Link>
-          <Link to="/Sobre">Sobre</Link>
-          <Link to="/Projetos">Projetos</Link>
-        </nav>
+      <nav className='container'>
+        <Link to="/">Inicio</Link>
+        <Link to="/Sobre">Sobre</Link>
+        <Link to="/Projetos">Projetos</Link>
+      </nav>
 
 
 
-      </header>
+    </header >
 
     </>
   )
--- src/Components/Projetos.jsx
+++ src/Components/Projetos.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import Footer from './Footer';
+import "../Style/project.scss";
+import "../Style/reset.scss";
 
+import Calculadora from "../assets/calculadora.png"
+import Fantastika from "../assets/fantastika.png";
+import Saudavel from "../assets/saudavel.png";
+import Inverno from "../assets/winter.png"
+import Modelo from "../assets/modelo.png"
 
 
 function Projetos () {
 return(
   <>
-  <main>
-   <h2> Meu projetos com deploy no Vercel :</h2>
-    <section>
-      <a><img src="" alt="" /></a>
-      <a><img src="" alt="" /></a>
-      <a><img src="" alt="" /></a>
-      <a><img src="" alt="" /></a>
-      <a><img src="" alt="" /></a>
-    </section>
+  <main className='container-pai'>
+   <h2> Uma olhadinha nos meus projetos:</h2>
+    <figure className='container-filho'>
+      <a href='https://github.com/micoliveira/calculadora-VNW'><img src={Calculadora} alt="" /></a>
+      <a href='https://github.com/micoliveira/Fantastika'><img src={Fantastika} alt="" /></a>
+      <a href='https://github.com/micoliveira/One-page-saudavel'><img src={Saudavel} alt="" /></a>
+      <a href='https://github.com/micoliveira/ProjetoWinter-DesafioVnW'><img src={Inverno} alt="" /></a>
+      <a href='https://github.com/micoliveira/projeto_annabela'><img src={Modelo} alt="" /></a>
+    </figure>
 
   </main>
   <Footer/>
